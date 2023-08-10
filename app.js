4b702611c72076f8cf53493103a39b5d5a0464c4
qiushihao <qiushihao@outlook.com>
2023-08-10 15:39:36 +0800

    first version

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express()
+const port = 9000
+
+app.use('/', createProxyMiddleware({
+    target: 'https://api.openai.com',
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+        proxyReq.removeHeader('x-forwarded-for');
+        proxyReq.removeHeader('x-real-ip');
+    },
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
+
+app.listen(port, () => {
+    console.log(`Proxy agent started http://localhost:${port}`)
+})
